fb3252b768c41277f59eda6c148383ba07c458f5
therickybobbeh <rcole2597@gmail.com>
2024-08-21 19:06:37 -0400

    initial commit

---
+++ Agent.py
@@ -0,0 +1,44 @@
+
+# To activate image processing, uncomment the following imports:
+# from PIL import Image
+# import numpy as np
+# import cv2
+
+class Agent:
+    def __init__(self):
+        """
+        The default constructor for your Agent. Make sure to execute any processing necessary before your Agent starts
+        solving problems here. Do not add any variables to this signature; they will not be used by main().
+        
+        This init method is only called once when the Agent is instantiated 
+        while the Solve method will be called multiple times. 
+        """
+        pass
+
+    def Solve(self, problem):
+        """
+        Primary method for solving incoming Raven's Progressive Matrices.
+
+        Args:
+            problem: The RavensProblem instance.
+
+        Returns:
+            int: The answer (1-6 for 2x2 OR 1-8 for 3x3) : Remember that the Autograder will have up to 2 additional images for answers.
+            Return a negative number to skip a problem.
+            Remember to return the answer [Key], not the name, as the ANSWERS ARE SHUFFLED in Gradescope.
+        """
+
+        '''
+        DO NOT use absolute file pathing to open files.
+        
+        Example: Read the 'A' figure from the problem using Pillow
+            image_a = Image.open(problem.figures["A"].visualFilename)
+            
+        Example: Read the '1' figure from the problem using OpenCv
+            image_1 = cv2.imread(problem.figures["1"].visualFilename)
+            
+        Don't forget to uncomment the imports as needed!
+        '''
+
+        # Placeholder: Skip all problems for now.
+        return -1
