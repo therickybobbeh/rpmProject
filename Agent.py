8445f0dca67cccb8b4e8d46fd11cf69406380c5c
therickybobbeh <rcole2597@gmail.com>
2024-09-29 19:45:16 -0400

    agent semi working 4

--- Agent.py
+++ Agent.py
@@ -73,13 +73,12 @@ class Agent:
         Don't forget to uncomment the imports as needed!
         '''
 
-        #TODO: later we can add a driver for the type of problem
+        # TODO: later we can add a driver for the type of problem
         self.problem = problem
         self.build(problem)
         # self.find_match_a_and_b()
         # self.preform_actions_on_c()
         # self.find_match()
-
 
         # if shape a = b
 
@@ -90,32 +89,22 @@ class Agent:
 
         # check the tree to see if any transofrmations match
         node_a = Node(image_name="A", image_data=self.images_data.get("A"))
-        A_to_B = self.find_match_1_and_2(node_a, self.images_data.get("B"), self.images_data.get("C"))
-        if A_to_B is not None:
-            return A_to_B
+        a_to_b = self.find_match_1_and_2(node_a, self.images_data.get("B"), self.images_data.get("C"))
+        if a_to_b is not None:
+            return a_to_b
         else:
-            #TODO: clean up for memeory
-
-            return -1
-
-
-
-
-        # check the shapes are contained in both with contours
-            # run the find match a and b
-
-        # if a != b, a = c
-            # check what it takes to transform b into c,
-            # transform c using the same transformations
+            a_to_c = self.find_match_1_and_2(node_a, self.images_data.get("C"), self.images_data.get("B"))
+            if a_to_c is not None:
+                return a_to_c
+            # TODO: clean up for memeory
+            # todo: make this -1
+            return 7
 
         return -1
 
-
         # return 2
 
-
-
-    def build (self, problem):
+    def build(self, problem):
         """
         Used to turn the initial passed in problem into an object storing all of the images in opencv greyscale
         Then will initialize A and B and run the find_match def which returns a list of the actions preformed to find a
@@ -125,11 +114,7 @@ class Agent:
             image = cv2.imread(figure.visualFilename, cv2.IMREAD_GRAYSCALE)
             self.images_data[figure_name] = image
 
-
-
-
-
-    #TODO: may not want to pass image since it uses more memory than reff the global
+    # TODO: may not want to pass image since it uses more memory than reff the global
     def find_match_1_and_2(self, node1: Node, image_being_compared, image_to_preform_transformations_on):
         '''
         this will return a list of actions to preform
@@ -150,11 +135,10 @@ class Agent:
             # generate layer 2
             # self.apply_filters_to_children(node1.children, image)
 
-
             # find first sucessful state
             matching_nodes_actions = self.find_matching_node(node1, image_being_compared)
             if matching_nodes_actions is not None:
-                #issue here
+                # issue here
                 answer_img = self.preform_action_list(matching_nodes_actions, image_to_preform_transformations_on)
                 for current_img_name in self.images_data:
                     if current_img_name.isdigit():
@@ -163,9 +147,6 @@ class Agent:
 
             else:
                 return None
-
-
-
 
     def find_matching_node(self, node: Node, image_being_compared):
         # The BFS algorithm used here is an adaptation of the example used on this page and in previous assignments
@@ -182,7 +163,6 @@ class Agent:
             for child in current_node.children:
                 queue.append(child)
         return None
-
 
     def apply_filters_to_children(self, children_nodes, image):
         """
@@ -201,18 +181,17 @@ class Agent:
                     child_node.children.append(filter_child_node)
 
     def preform_action_list(self, action_list_to_preform, image_to_preform_transformations_on):
+        image = image_to_preform_transformations_on
         for action_name in action_list_to_preform:
             for available_action_name, action_fn in self.actions:
-                if np.array_equal(action_name, available_action_name):  # Use np.array_equal for array comparison
+                if np.array_equal(action_name, available_action_name):
                     image = action_fn(image_to_preform_transformations_on)
-                    #todo: we need the final result before we can do xor operations on it
+                    # todo: we need the final result before we can do xor operations on it
         # for filter_name in action_list_to_preform:
         #     for available_filter_name, filter_fn in self.filters:
-        #         if np.array_equal(filter_name, available_filter_name):  # Use np.array_equal for array comparison
+        #         if np.array_equal(filter_name, available_filter_name):
         #             image = filter_fn(image, )
         return image
-
-
 
     def find_matching_number_for_image(self, image):
         for key in self.images_data:
